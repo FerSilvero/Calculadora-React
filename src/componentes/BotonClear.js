e0701217dde91e95da668681333c93c8403db4fb
Fernando Silvero <ferpolasebas@gmail.com>
2023-03-14 13:00:04 -0300

    upload

---
+++ src/componentes/BotonClear.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../hojas-de-estilo/BotonClear.css'
+
+const BotonClear = (props) => {
+  return (
+    <div className='boton-clear' onClick={props.manejarClear}>
+        {props.children}
+    </div>
+  )
+}
+
+export default BotonClear
